88e72434e9d1c807aaaac4498452dbd3b1fda26b
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-15 22:01:14 +0200

    feat: separate configuration

---
+++ src/FirebaseFunctionsRateLimiterConfiguration.ts
@@ -0,0 +1,29 @@
+import ow from "ow";
+
+export interface FirebaseFunctionsRateLimiterConfiguration {
+    firebaseCollectionKey?: string;
+    periodSeconds?: number;
+    maxCallsPerPeriod?: number;
+}
+
+export namespace FirebaseFunctionsRateLimiterConfiguration {
+    export interface ConfigurationFull {
+        firebaseCollectionKey: string;
+        periodSeconds: number;
+        maxCallsPerPeriod: number;
+    }
+
+    export namespace ConfigurationFull {
+        export function validate(o: ConfigurationFull) {
+            ow(o.firebaseCollectionKey, "configuration.FirebaseFunctionsRateLimiter", ow.string.alphanumeric.nonEmpty);
+            ow(o.periodSeconds, "configuration.periodSeconds", ow.number.integer.finite.greaterThan(0));
+            ow(o.maxCallsPerPeriod, "configuration.maxCallsPerPeriod", ow.number.integer.finite.greaterThan(0));
+        }
+    }
+
+    export const DEFAULT_CONFIGURATION: ConfigurationFull = {
+        firebaseCollectionKey: "rate_limiter_1",
+        periodSeconds: 5 * 60,
+        maxCallsPerPeriod: 1,
+    };
+}
