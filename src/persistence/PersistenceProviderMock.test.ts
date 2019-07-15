caf3f3d2eaef7193311aa49ee60b874c3d889fdc
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-16 00:16:36 +0200

    test: fix object key management in PersistenceProviderMock

--- src/persistence/PersistenceProviderMock.test.ts
+++ src/persistence/PersistenceProviderMock.test.ts
@@ -4,20 +4,26 @@ import { PersistenceProvider } from "./PersistenceProvider";
 import { PersistenceRecord } from "./PersistenceRecord";
 
 export class PersistenceProviderMock implements PersistenceProvider {
-    private persistenceObject: string[] = [];
+    public persistenceObject: { [x: string]: PersistenceRecord } = {};
 
     public async runTransaction(asyncTransactionFn: () => Promise<void>): Promise<void> {
         await asyncTransactionFn();
     }
 
     public async getRecord(collectionName: string, recordName: string): Promise<PersistenceRecord> {
-        await BluebirdPromise.delay(4);
-        return this.persistenceObject[collectionName + "_" + recordName] || this.createEmptyRecord();
+        await BluebirdPromise.delay(2);
+        const key = this.getKey(collectionName, recordName);
+        return this.persistenceObject[key] || this.createEmptyRecord();
     }
 
     public async saveRecord(collectionName: string, recordName: string, record: PersistenceRecord): Promise<void> {
-        await BluebirdPromise.delay(4);
-        this.persistenceObject[collectionName + "_" + recordName] = record;
+        await BluebirdPromise.delay(2);
+        const key = this.getKey(collectionName, recordName);
+        this.persistenceObject[key] = record;
+    }
+
+    private getKey(collectionName: string, recordName: string): string {
+        return collectionName + "_" + recordName;
     }
 
     private createEmptyRecord(): PersistenceRecord {
