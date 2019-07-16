1dc185a3f5ab084c268ef9644940fdf76e0315cf
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-17 00:41:46 +0200

    fix: fix compile error in FirebaseFunctionsRateLimiter class

--- src/FirebaseFunctionsRateLimiter.ts
+++ src/FirebaseFunctionsRateLimiter.ts
@@ -33,7 +33,7 @@ export class FirebaseFunctionsRateLimiter {
             configurationFull,
             persistenceProvider,
             timestampProvider,
-            debugFn,
+            this.debugFn,
         );
     }
 
