56a5940a05bec78aff1ce6cbb992bd3a2d46c4aa
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-16 13:03:42 +0200

    test: fix test runner failures

--- scripts/run-tests-with-firebase-emulator.sh
+++ scripts/run-tests-with-firebase-emulator.sh
@@ -1,6 +1,6 @@
     
 #!/usr/bin/env bash
-set -e # fail on first error
+set +e # do not fail on first error
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." # parent dir of scripts dir
 cd "${DIR}"
 
