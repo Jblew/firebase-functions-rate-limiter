c5097377d5715c8dee4a1d641f57c9efd96741f1
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-16 13:56:13 +0200

    ci: make integration test runner exit with proper codes

--- scripts/run-integration-tests-with-firebase-emulator.sh
+++ scripts/run-integration-tests-with-firebase-emulator.sh
@@ -4,11 +4,12 @@ set +e # do not fail on first error
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." # parent dir of scripts dir
 cd "${DIR}"
 
-echo "Begin tests"
+echo "Begin tests in ${DIR}"
 
     firebase serve --only firestore & 
-    export FBPID=$! && 
-    sleep 5 && npm run do_verify ; 
+    FBPID=$! && 
+    sleep 5 && npm run do_verify ; TEST_EXIT_CODE=$? ; 
     echo "Killing firebase emulator server: $FBPID" && kill -STOP $FBPID 
 
-echo "Tests done"
+echo "Tests done, exit with ${TEST_EXIT_CODE}"
+exit $TEST_EXIT_CODE
