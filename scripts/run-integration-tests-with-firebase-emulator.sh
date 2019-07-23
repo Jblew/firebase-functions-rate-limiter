a1ff22e120b8dd08b47ca95979ea019d0c254cd0
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-07-23 17:48:37 +0200

    ci: run tests with emulators:exec instead of start

--- scripts/run-integration-tests-with-firebase-emulator.sh
+++ scripts/run-integration-tests-with-firebase-emulator.sh
@@ -1,15 +1,9 @@
     
 #!/usr/bin/env bash
-set +e # do not fail on first error
+set -e # fail on first error
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." # parent dir of scripts dir
 cd "${DIR}"
 
 echo "Begin tests in ${DIR}"
 
-    firebase serve --only firestore & 
-    FBPID=$! && 
-    sleep 4 && npm run do_verify ; TEST_EXIT_CODE=$? ; 
-    echo "Killing firebase emulator server: $FBPID" && kill -STOP $FBPID 
-
-echo "Tests done, exit with ${TEST_EXIT_CODE}"
-exit $TEST_EXIT_CODE
+firebase emulators:exec --only firestore "npm run do_verify"
