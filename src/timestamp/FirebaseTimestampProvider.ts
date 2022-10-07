f59944b0d216e919bfd7b4cce659329598bdcef8
Jean Costa <jean.adcc@gmail.com>
2022-10-07 14:31:10 -0300

    refactor(TimestampProvider): use milliseconds instead of seconds

--- src/timestamp/FirebaseTimestampProvider.ts
+++ src/timestamp/FirebaseTimestampProvider.ts
@@ -3,7 +3,7 @@ import * as admin from "firebase-admin";
 import { TimestampProvider } from "./TimestampProvider";
 
 export class FirebaseTimestampProvider implements TimestampProvider {
-    public getTimestampSeconds(): number {
-        return admin.firestore.Timestamp.now().seconds;
+    public getTimestampMs(): number {
+        return admin.firestore.Timestamp.now().toMillis();
     }
 }
--- src/timestamp/TimestampProviderMock.test.ts
+++ src/timestamp/TimestampProviderMock.test.ts
@@ -1,14 +1,14 @@
 import { TimestampProvider } from "./TimestampProvider";
 
 export class TimestampProviderMock implements TimestampProvider {
-    private timestampNowSeconds: number | undefined = undefined;
+    private timestampNowMs: number | undefined = undefined;
 
-    public getTimestampSeconds(): number {
-        if (!this.timestampNowSeconds) return Date.now() / 1000;
-        else return this.timestampNowSeconds;
+    public getTimestampMs(): number {
+        if (!this.timestampNowMs) return Date.now();
+        else return this.timestampNowMs;
     }
 
     public setTimestampSeconds(timestampSeconds: number) {
-        this.timestampNowSeconds = timestampSeconds;
+        this.timestampNowMs = timestampSeconds * 1000;
     }
 }
